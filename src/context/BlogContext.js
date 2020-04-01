c63af7a229de3ad8eefd91d387baa6a3041ce1eb
Luiz Castro <lfernando.cwb@gmail.com>
2020-03-31 22:08:15 -0300

    post form component

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,10 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'edit_blogpost':
+            return state.map(blogPost => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost;
+            });
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
@@ -10,29 +14,42 @@ const blogReducer = (state, action) => {
                 id: Math.floor(Math.random() * 999999),
                 title: action.payload.title,
                 content: action.payload.content
-            }
-            ];
+            }];
         default:
             return state;
-    }
+    };
 };
 
 const addBlogPost = dispatch => {
     return (title, content, callback) => {
         dispatch({ type: 'add_blogpost', payload: { title, content } });
-        callback();
-    }
+        if (callback) {
+            callback();
+        }
+    };
 };
 
 const deleteBlogPost = dispatch => {
     return id => {
         dispatch({ type: 'delete_blogpost', payload: id });
     };
-}
+};
+
+const editBlogPost = dispatch => {
+    return (id, title, content, callback) => {
+        dispatch({
+            type: 'edit_blogpost',
+            payload: { id, title, content }
+        });
+        if (callback) {
+            callback();
+        }
+    };
+};
 
 export const { Context, Provider } = createDataContext(
     blogReducer,
-    { addBlogPost, deleteBlogPost },
+    { addBlogPost, deleteBlogPost, editBlogPost },
     [{ title: 'Test Post', content: 'Test Content', id: 1 }]
 );
 
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,54 +1,30 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext'
+import BlogPostForm from '../components/BlogPostForm';
+
 
 const EditScreen = ({ navigation }) => {
-    const { state } = useContext(Context)
+    const id = navigation.getParam('id')
+    const { state, editBlogPost } = useContext(Context)
 
     const blogPost = state.find(
-        (blogPost) => blogPost.id === navigation.getParam('id')
+        blogPost => blogPost.id === id
     );
 
-    const [title, setTitle] = useState(blogPost.title);
-    const [content, setContent] = useState(blogPost.content);
-
     return (
-        <View>
-            <Text style={styles.label} >Enter New Title: </Text>
-            <TextInput
-                style={styles.input}
-                value={title} onChangeText={(text) => setTitle(text)}
-            />
-            <Text style={styles.label}  >Enter New Content: </Text>
-            <TextInput
-                style={styles.input} value={content}
-                onChangeText={(text) => setContent(text)}
-            />
-            <Button title="Add Blog Post"
-                onPress={() => addBlogPost(title, content, () => {
-                    navigation.navigate('Index');
-                })
-                }
-            />
-        </View>
+        <BlogPostForm
+            initialValues={{ title: blogPost.title, content: blogPost.content }}
+            onSubmit={(title, content) => {
+                editBlogPost(id, title, content, () => {
+                    navigation.pop();
+                });
+            }}
+        />
     )
 };
 
-const styles = StyleSheet.create({
-    input: {
-        fontSize: 18,
-        borderWidth: 1,
-        borderColor: 'black',
-        marginBottom: 10,
-        padding: 5,
-        margin: 5,
-    },
-    label: {
-        fontSize: 20,
-        marginBottom: 5,
-        marginLeft: 5
-    }
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
 
